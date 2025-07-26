cbadada0d948b119b19cd843c3f229b7620a4ca8
Nick <poezzdd@gmail.com>
2025-07-26 17:43:48 +0300

    UIClasses

---
+++ Assets/Minesweeper/UI/Realization/UIGameWindow.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Minesweeper.UI.Realization
+{
+    public class UIGameWindow : MonoBehaviour
+    {
+        public event Action OnRestartButtonClickedEvent;
+        
+        public TextMeshProUGUI TimerText => timerText;
+        public TextMeshProUGUI MinesLeftText => minesLeftText;
+        
+        [SerializeField] private Button restartButton;
+        
+        [SerializeField] private GameObject winText;
+        [SerializeField] private GameObject loseText;
+        
+        [SerializeField] private GameObject gameSmile;
+        [SerializeField] private GameObject winSmile;
+        [SerializeField] private GameObject loseSmile;
+        
+        [SerializeField] private TextMeshProUGUI timerText;
+        
+        [SerializeField] private TextMeshProUGUI minesLeftText;
+
+        [SerializeField] private Transform gridTransform;
+
+        public void SetWinState()
+        {
+            ClearStates();
+            
+            winSmile.SetActive(true);
+            winText.SetActive(true);
+        }
+        
+        public void SetLoseState()
+        {
+            ClearStates();
+            
+            loseSmile.SetActive(true);
+            loseText.SetActive(true);
+        }
+
+        public void SetGameState()
+        {
+            ClearStates();
+            
+            gameSmile.SetActive(true);
+        }
+
+        private void ClearStates()
+        {
+            gameSmile.SetActive(false);
+            winSmile.SetActive(false);
+            loseSmile.SetActive(false);
+            
+            winText.SetActive(false);
+            loseText.SetActive(false);
+        }
+
+        private void Start()
+        {
+            restartButton.onClick.AddListener(OnRestartButtonClicked);
+        }
+
+        private void OnRestartButtonClicked()
+        {
+            OnRestartButtonClickedEvent?.Invoke();
+        }
+    }
+}
---
+++ Assets/Minesweeper/UI/Realization/UIGameWindowController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Minesweeper.Application;
+using Minesweeper.GameStarter.Interfaces;
+using Minesweeper.GameTimer.Interfaces;
+using Minesweeper.MineField.Interfaces;
+using UniRx;
+
+namespace Minesweeper.UI.Realization
+{
+    public class UIGameWindowController : IDisposable
+    {
+        private UIGameWindow _uiGameWindow;
+        private IGameStarter _gameStarter;
+
+        private CompositeDisposable _compositeDisposable = new CompositeDisposable();
+        
+        public UIGameWindowController(
+            GameSceneData gameSceneData,
+            IGameTimer gameTimer,
+            IMineFieldController mineFieldController,
+            IGameStarter gameStarter)
+        {
+            _uiGameWindow = gameSceneData.UIGameWindow;
+            _gameStarter = gameStarter;
+
+            _compositeDisposable.Add(gameTimer.TotalSeconds.Subscribe(SetTimerText));
+            
+            _compositeDisposable.Add(
+                mineFieldController
+                    .MinesLeft
+                    .Subscribe(minesLeft => _uiGameWindow.MinesLeftText.text = minesLeft.ToString()));
+            
+            _uiGameWindow.OnRestartButtonClickedEvent += OnRestartButtonClicked;
+
+            _gameStarter.OnGameStartedEvent += OnStartGame;
+            _gameStarter.OnGameEndedEvent += OnEndGame;
+        }
+
+        public void Dispose()
+        {
+            _compositeDisposable.Dispose();
+            _compositeDisposable = null;
+            
+            _uiGameWindow.OnRestartButtonClickedEvent -= OnRestartButtonClicked;
+            _uiGameWindow = null;
+            
+            _gameStarter.OnGameStartedEvent -= OnStartGame;
+            _gameStarter.OnGameEndedEvent -= OnEndGame;
+            _gameStarter = null;
+        }
+
+        private void SetTimerText(int seconds)
+        {
+            _uiGameWindow.TimerText.text = $"{seconds / 60 :00}:{seconds % 60 :00}";
+        }
+
+        private void OnRestartButtonClicked()
+        {
+            _gameStarter.StartGame();
+        }
+
+        private void OnStartGame()
+        {
+            _uiGameWindow.SetGameState();
+        }
+
+        private void OnEndGame(bool isWin)
+        {
+            if (isWin)
+            {
+                _uiGameWindow.SetWinState();
+            }
+            else
+            {
+                _uiGameWindow.SetLoseState();
+            }
+        }
+    }
+}
