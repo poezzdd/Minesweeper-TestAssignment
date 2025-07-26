3a538b2c00b13a094613614916a746a6c60fa843
Nick <poezzdd@gmail.com>
2025-07-26 17:43:58 +0300

    GameStarter

---
+++ Assets/Minesweeper/GameStarter/Interfaces/IGameStarter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Minesweeper.GameStarter.Interfaces
+{
+    public interface IGameStarter
+    {
+        event Action OnGameStartedEvent;
+        event Action<bool> OnGameEndedEvent;
+        void StartGame();
+    }
+}
---
+++ Assets/Minesweeper/GameStarter/Realization/GameStarter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Minesweeper.GameStarter.Interfaces;
+using Minesweeper.GameTimer.Interfaces;
+using Minesweeper.MineField.Interfaces;
+
+namespace Minesweeper.GameStarter.Realization
+{
+    public class GameStarter : IGameStarter, IDisposable
+    {
+        public event Action OnGameStartedEvent;
+        public event Action<bool> OnGameEndedEvent;
+        
+        private IMineFieldController _mineFieldController;
+        private IGameTimer _gameTimer;
+
+        public GameStarter(
+            IMineFieldController mineFieldController,
+            IGameTimer gameTimer)
+        {
+            _mineFieldController = mineFieldController;
+            _gameTimer = gameTimer;
+
+            _mineFieldController.OnEndGameEvent += OnGameEnded;
+        }
+        
+        public void Dispose()
+        {
+            OnGameStartedEvent = null;
+            OnGameEndedEvent = null;
+            
+            _mineFieldController.OnEndGameEvent -= OnGameEnded;
+            _mineFieldController = null;
+
+            _gameTimer = null;
+        }
+        
+        public void StartGame()
+        {
+            _mineFieldController.Start();
+            _gameTimer.Start();
+            
+            OnGameStartedEvent?.Invoke();
+        }
+
+        private void OnGameEnded(bool isWin)
+        {
+            _gameTimer.Stop();
+            
+            OnGameEndedEvent?.Invoke(isWin);
+        }
+    }
+}
